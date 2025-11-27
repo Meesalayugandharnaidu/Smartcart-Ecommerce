13bfe94aa122351af0973f8d6b5ccc444f8a6a09
Meesala Yugandhar Naidu <meesalayugandharnaidu@gmail.com>
2025-11-27 21:49:21 +0530

    y

---
+++ config.py
@@ -0,0 +1,18 @@
+SECRET_KEY = "Naidu@123"
+
+# MySQL Database
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "Naidu@123"
+DB_NAME = "smartcart_db"
+
+# Email SMTP Settings
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'meesalayugandharnaidu@gmail.com'
+MAIL_PASSWORD = 'ghqs lbho hfvt teli'   # Gmail App Password
+
+
+RAZORPAY_KEY_ID = "rzp_test_RjUGK6hO0z5ZU9"
+RAZORPAY_KEY_SECRET = "BpaLXGCGbqqFJtjJETnv7a37"
